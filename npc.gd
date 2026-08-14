c27c065ac028a449e4f8d40cd89f8d5f27a8a2be
AcunaJose23 <joseandresacuna2309@gmail.com>
2026-08-14 16:49:20 -0600

    Refactor NPC dialogue handling to ensure dialogue only shows when active

--- npc.gd
+++ npc.gd
@@ -1,16 +1,20 @@
 extends CharacterBody2D
 
 var player_near = false
+var player_node = null
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		player_near = true
+		player_node = body
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
 		player_near = false
+		player_node = null
 
 func _process(_delta: float) -> void:
 	if player_near and Input.is_action_just_pressed("talk"):
-		DialogueManager.show_dialogue_balloon(load("res://Try.dialogue"), "start")
+		if not player_node.dialogue_active:
+			DialogueManager.show_dialogue_balloon(load("res://Try.dialogue"), "start")
 	
