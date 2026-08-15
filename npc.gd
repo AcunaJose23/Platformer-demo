f1fa82e32b575d8476c1b4fdb521e2aa2d92c639
AcunaJose23 <joseandresacuna2309@gmail.com>
2026-08-14 22:04:32 -0600

    Update coin animation frame progress and adjust score calculation in NPC dialogue

--- npc.gd
+++ npc.gd
@@ -16,5 +16,6 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 func _process(_delta: float) -> void:
 	if player_near and Input.is_action_just_pressed("talk"):
 		if not player_node.dialogue_active:
+			Global.calcular_puntaje(player_node.time_elapsed)
 			DialogueManager.show_dialogue_balloon(load("res://Try.dialogue"), "start")
 	
